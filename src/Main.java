31d982288e6c986cb6fa2e0bab61c2e13f05e324
Slava Kibanov <kibanchous@mail.ru>
2023-10-06 12:36:03 +0500

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
-public class Main { public static void main(String[] args) {
-    int amountnow = 100; // Сумма на счету
-    int bonus = 1000; // Количество рублей для получения бонуса
-    int dep = 1121; // Сумма пополнения
-    if (dep > bonus) {
-        System.out.println("Начисленно бонусов - " + (dep / 100) + ", итоговая сумма на счете - " + (amountnow + (dep / 100 + dep) + " рублей"));
-    } else {
-        System.out.println("Итоговая сумма на счете - " + (amountnow + dep) + " рублей");
+public class Main {
+    public static void main(String[] args) {
+        int amountNow = 100; // Сумма на счету
+        int bonus = 1000; // Количество рублей для получения бонуса
+        int dep = 1121; // Сумма пополнения
+        if (dep > bonus) {
+            System.out.println("Начисленно бонусов - " + (dep / 100) + ", итоговая сумма на счете - " + (amountNow + (dep / 100 + dep) + " рублей"));
+        } else {
+            System.out.println("Итоговая сумма на счете - " + (amountNow + dep) + " рублей");
+        }
     }
-
-}
-
-
 }
